78abbc071106e2dfb10d777e6a31665378e89cce
Schultec <97144271+Schultec@users.noreply.github.com>
2022-01-16 14:02:34 -0800

    Add files via upload

---
+++ A1_Collin_Schulte.py
@@ -0,0 +1,15 @@
+#collect integer 1
+num1 = int(input("enter first nonzero integer: "))
+#collect integer 2
+num2 = int(input("enter second nonzero integer: "))
+#find and display sum
+print(num1, "plus", num2, "equals" ,num1+num2,)
+#find and display differance
+print(num1, "minus", num2, "equals" ,num1-num2,)
+#find and display num1*num2
+print(num1, "multiplyed by", num2, "equals" ,num1*num2,)
+#find and display num1 raised to the num2
+print(num1, "raised by", num2, "equals" ,num1^num2,)
+#find the integer quotient of the numbers and the remainder before displaying both
+print(num1, "divided by", num2, "equals" ,num1//num2, "with a remainder of" ,num1%num2)
+input()
